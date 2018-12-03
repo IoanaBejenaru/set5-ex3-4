367a7430d35b16426d191bf95e07fbd420780c89
IoanaBejenaru <44978610+IoanaBejenaru@users.noreply.github.com>
2018-12-03 18:58:15 +0200

    Add files via upload

---
+++ bejenaruioana_4set5.cpp.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+using namespace std
+unsigned int n;
+int biti=0;
+int main()
+{
+    cin>>n;
+    if(n==o)
+    {
+        cout<<"1"
+    }
+    else
+    {
+        if(n>0)
+
+    while(n!=0)
+    {
+        int biti;
+        n=n>>1;
+        biti++;
+    }
+
+    else
+        if(n==-1){
+            cout<<1;
+            return 0;
+        }
+    else
+        while(n<=1)
+    {
+        n=n>>1;
+        biti++;
+    }
+    cout<<biti;
+return 0;}
